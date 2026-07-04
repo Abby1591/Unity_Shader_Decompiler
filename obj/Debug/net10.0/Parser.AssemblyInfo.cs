b3fcc5ac6bf6d474f80d1028980864644f10c928
Abby1591 <abhyudayprabhakar2a@gmail.com>
2026-07-04 21:25:26 +0530

    Expanded Opcode Table

--- obj/Debug/net10.0/Parser.AssemblyInfo.cs
+++ obj/Debug/net10.0/Parser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5acc8437738ea8e52cb3c7334e2be104a2b0bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5932b9d508a171019dda649c522315f451753537")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
