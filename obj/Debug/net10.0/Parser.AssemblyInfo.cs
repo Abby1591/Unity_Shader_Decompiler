7d6dda944404aadf692c3a17980a634c0b259d8a
Abby1591 <abhyudayprabhakar2a@gmail.com>
2026-07-08 15:38:03 +0530

    Added More Opcodes and fixed existing ones

--- obj/Debug/net10.0/Parser.AssemblyInfo.cs
+++ obj/Debug/net10.0/Parser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfbdd5f528dcf975dd04ac617b4ac2381f1540c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a77a09e9cb4f52d64fb01773b645b23411ec15e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
