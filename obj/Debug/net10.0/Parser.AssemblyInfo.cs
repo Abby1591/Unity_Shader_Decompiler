79cbe1130fd478fab7e4c1021fba340c9fbd2c3f
Abby1591 <abhyudayprabhakar2a@gmail.com>
2026-07-31 22:14:19 +0530

    Fixed The Workflow

--- obj/Debug/net10.0/Parser.AssemblyInfo.cs
+++ obj/Debug/net10.0/Parser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143b9c76281b3cc4925c7f02a12eeb07ef089b34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea187a52ecd66016463d5dda27093ab7f91608c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
